346e2bd9b221ee2059c80639500fb14107eab899
TorokCsongor <torokcsongor20040831@gmail.com>
2026-02-21 13:03:50 +0100

    v1- basic UI and database connection

---
+++ HabitTracker/DatabaseHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitTracker
+{
+    public static class DatabaseHelper
+    {
+        private const string DbPath = "Data Source=habits.db";
+
+        public static void InitializeDatabase()
+        {
+            using (var connection = new SqliteConnection(DbPath))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"CREATE TABLE IF NOT EXISTS Habits (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT,
+                LastOccurrence DATETIME
+            )";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
